e937499109e249540dfd069efbcd713e38c6dac2
José Legatheaux Martins <jose.legatheaux@fct.unl.pt>
2020-10-11 21:14:12 +0100

    corrected Link toString() method

--- src/cnss/simulator/Link.java
+++ src/cnss/simulator/Link.java
@@ -2,6 +2,7 @@ package cnss.simulator;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 import cnss.simulator.Event.EventType;
 import cnss.simulator.Packet.PacketType;
@@ -29,12 +30,12 @@ public class Link {
 	private int counter2_in = 0;
 	private int counter1_out = 0;
 	private int counter2_out = 0;
-	
+
 	private Queue<Packet> in1 = new LinkedList<>();
 	private Queue<Packet> in2 = new LinkedList<>();
 	private Queue<Packet> out1 = new LinkedList<>();
 	private Queue<Packet> out2 = new LinkedList<>();
-	
+
 	// during a processing step, packets are transmitted; when the next one begins
 	// transmitting, may be the previous ones have not yet been fully transmitted.
 	private int timeOfLastBitTransmitted1 = 0;
@@ -49,6 +50,9 @@ public class Link {
 	private double errors = 0.0; // error rate in % - 0.0 is a perfect (no errors) link
 	private double jitter = 0.0; // in % - 0.0 is a link without jitter
 	private boolean up = true;
+
+	private Random randomDrop = null;
+	private Random randomJitt = null;
 
 	Simulator simulator; // the simulator where this link leaves.
 	// required to allow a link to create events of packet delivery to
@@ -85,6 +89,10 @@ public class Link {
 		jitter = j;
 		up = true;
 		simulator = s;
+
+		if ( errs > 0.0001 ) randomDrop = new Random(10000+n1+n2+i1+i2);
+		if ( jitter > 0.0001 ) randomJitt = new Random(20000+n1+n2+i1+i2);
+
 	}
 
 	/**
@@ -146,56 +154,25 @@ public class Link {
 	 * @param now is the current time
 	 */
 	public void transmitPackets(int now) {
-		Packet p;
 		if (isUp()) {
 			// begin by side 1 of the link
 			// packets will begin being transmitted now or when previous packets are done
 			if ( timeOfLastBitTransmitted1 < now ) timeOfLastBitTransmitted1 = now;
 			while (out1.size() > 0) {
-				p = out1.poll(); // retrieves the packet from the queue
-				// TODO: packet dropping and jitter
-
-				if (p.getType() == PacketType.TRACING) {
-					// add the link crossed to the path - time is when the packet will start being transmitted
-					String trace = p.getPayload().toString() + "time " 
-					+ timeOfLastBitTransmitted1 + " " + node1 + "." + iface1 + "->" + node2 + "." + iface2;
-					p.setPayload(trace.getBytes());
+				Packet p = out1.poll(); // retrieves the packet from the queue
+				if ( randomDrop != null ) {
+					if ( randomDrop.nextInt(10000) <= (int)(errors*10000) ) continue;
 				}
-				
-				double transmissionTime = ((double) p.getSize()) * 8.0 * 1000.0 / (double) bwidth; // in ms
-				int transitTime = (int) transmissionTime + latency;
-				if (transitTime < 1)
-					transitTime = 1; // this forces the treatment of the event
-				// in the next processing step, otherwise the event would be ignored
-				// System.out.println("TransmitPackets computed "+transitTime+" ms");
-				int deliverTime = timeOfLastBitTransmitted1+transitTime;
-				timeOfLastBitTransmitted1 += (int) transmissionTime;
-				outputEvents.add(new Event(EventType.DELIVER_PACKET, deliverTime, 0, null, p, node2, iface2));
-				counter2_in++; // the packet will be later received by node 2, interface 2
+				ProcessNextPacket1(p);
 			}
 			// now side 2
 			if ( timeOfLastBitTransmitted2 < now ) timeOfLastBitTransmitted2 = now;
 			while (out2.size() > 0) {
-				p = out2.poll(); // retrieves the packet from the queue
-				// TODO: packet dropping and jitter
-
-				if (p.getType() == PacketType.TRACING) {
-					// add the link crossed to the path - time is when the packet will start being transmitted
-					String trace = p.getPayload().toString() + "time " 
-					+ timeOfLastBitTransmitted2 + " " + node2 + "." + iface2 + "->" + node1 + "." + iface1;
-					p.setPayload(trace.getBytes());
+				Packet p = out2.poll(); // retrieves the packet from the queue
+				if ( randomDrop != null ) {
+					if ( randomDrop.nextInt(10000) <= (int)(errors*10000) ) continue;
 				}
-				
-				double transmissionTime = ((double) p.getSize()) * 8.0 * 1000.0 / (double) bwidth; // in ms
-				int transitTime = (int) transmissionTime + latency;
-				if (transitTime < 1)
-					transitTime = 1; // this forces the treatment of the event
-				// in the next processing step, otherwise the event would be ignored
-				// System.out.println("TransmitPackets computed "+transitTime+" ms");
-				int deliverTime = timeOfLastBitTransmitted2+transitTime;
-				timeOfLastBitTransmitted2 += (int) transmissionTime;
-				outputEvents.add(new Event(EventType.DELIVER_PACKET, deliverTime, 0, null, p, node1, iface1));
-				counter1_in++; // the packet will be later received by node 1, interface 1
+				ProcessNextPacket2(p);
 			}
 		} else {
 			// the link is down, output queues should be reset if not yet
@@ -207,6 +184,58 @@ public class Link {
 			System.exit(-1);
 		}
 	}
+
+	/**
+	 * Processes one packet sent from side 1 of the link
+	 * 
+	 * p the Packet to be processed
+	 */
+	void ProcessNextPacket1 (Packet p) {
+		if (p.getType() == PacketType.TRACING) {
+			// add the link crossed to the path - time is when the packet will start being transmitted
+			String trace = p.getPayload().toString() + "time " 
+					+ timeOfLastBitTransmitted1 + " " + node1 + "." + iface1 + "->" + node2 + "." + iface2;
+			p.setPayload(trace.getBytes());
+		}
+		double transmissionTime = ((double) p.getSize()) * 8.0 * 1000.0 / (double) bwidth; // all in ms
+		double varLat = 0.0;
+		if ( randomJitt != null ) varLat = (double) randomJitt.nextInt(10000)/10000 * jitter * transmissionTime;
+		int transitTime = (int) transmissionTime + latency + (int) varLat;
+		// transitTime must be at least 1 to force the transmission in a future processing step
+		if (transitTime < 1) transitTime = 1;
+		// System.out.println("TransmitPackets computed "+transitTime+" ms");
+		int deliverTime = timeOfLastBitTransmitted1+transitTime;
+		timeOfLastBitTransmitted1 += (int) transmissionTime;
+		counter2_in++; // the packet will be later received by node 2, interface 2
+		outputEvents.add(new Event(EventType.DELIVER_PACKET, deliverTime, 0, null, p, node2, iface2));
+	}
+
+
+	/**
+	 * Processes one packet sent from side 2 of the link
+	 * 
+	 * p the Packet to be processed
+	 */
+	void ProcessNextPacket2 (Packet p) {
+		if (p.getType() == PacketType.TRACING) {
+			// add the link crossed to the path - time is when the packet will start being transmitted
+			String trace = p.getPayload().toString() + "time " 
+					+ timeOfLastBitTransmitted2 + " " + node2 + "." + iface2 + "->" + node1 + "." + iface1;
+			p.setPayload(trace.getBytes());
+		}
+		double transmissionTime = ((double) p.getSize()) * 8.0 * 1000.0 / (double) bwidth; // all in ms
+		double varLat = 0.0;
+		if ( randomJitt != null ) varLat = (double) randomJitt.nextInt(10000)/10000 * jitter * transmissionTime;
+		int transitTime = (int) transmissionTime + latency + (int) varLat;
+		// transitTime must be at least 1 to force the transmission in a future processing step
+		if (transitTime < 1) transitTime = 1;
+		// System.out.println("TransmitPackets computed "+transitTime+" ms");
+		int deliverTime = timeOfLastBitTransmitted2+transitTime;
+		timeOfLastBitTransmitted2 += (int) transmissionTime;
+		counter1_in++; // the packet will be later received by node 1, interface 1
+		outputEvents.add(new Event(EventType.DELIVER_PACKET, deliverTime, 0, null, p, node1, iface1));
+	}
+
 
 	/**
 	 * Return an output <code>Event</code> generated by the transmission of packets
@@ -255,25 +284,25 @@ public class Link {
 		return latency;
 	}
 
-//	/**                                                                         
-//	 * Returns a <code>Packet</code>, from the in bound queue for the 
-//	 * node specified by node id. If no packet is present returns null.
-//	 * @param nodeid the node whose in bound queue to remove the packet from.                                                    
-//	 * @return the packet being retrieved.   
-//	 */
-//	public Packet dequeuePackets(int nodeid)
-//	{
-//		if (nodeid == node1) {
-//			if ( in1.size() > 0 ) return in1.poll(); else return null;
-//		}
-//		else if (nodeid == node2) {
-//			if ( in2.size() > 0 ) return in2.poll(); else return null;
-//		} else {
-//			System.err.println("dequeue packets - impossible nodeid");
-//			System.exit(-1);
-//		}
-//		return null;
-//	}
+	//	/**                                                                         
+	//	 * Returns a <code>Packet</code>, from the in bound queue for the 
+	//	 * node specified by node id. If no packet is present returns null.
+	//	 * @param nodeid the node whose in bound queue to remove the packet from.                                                    
+	//	 * @return the packet being retrieved.   
+	//	 */
+	//	public Packet dequeuePackets(int nodeid)
+	//	{
+	//		if (nodeid == node1) {
+	//			if ( in1.size() > 0 ) return in1.poll(); else return null;
+	//		}
+	//		else if (nodeid == node2) {
+	//			if ( in2.size() > 0 ) return in2.poll(); else return null;
+	//		} else {
+	//			System.err.println("dequeue packets - impossible nodeid");
+	//			System.exit(-1);
+	//		}
+	//		return null;
+	//	}
 
 	/**
 	 * Returns the queue length for a particular direction and side of the link.
@@ -305,7 +334,7 @@ public class Link {
 		String state = up ? "up" : "down";
 		String s = "Link (Node1:" + node1 + " I1:" + iface1 + ")";
 		s = s + "<-->";
-		s = s + "(Node2:" + node2 + " I2:" + iface2 + ") bwd: " + bwidth + " bps lat: " + latency + " ms error %: " + errors + " jit %: " + jitter + " "
+		s = s + "(Node2:" + node2 + " I2:" + iface2 + ") bwd: " + bwidth + " bps lat: " + latency + " ms error: " + errors + " jit: " + jitter + " "
 				+ state;
 		return s;
 	}
